9a3915fbdb4a039f92c1ce7ea39c6197d1c152fe
aissata <aimaiga2@gmail.com>
2022-07-28 09:41:48 +0200

    changed root for files

---
+++ sender/src/bin/send.rs
@@ -0,0 +1,42 @@
+use std::time::{Duration, Instant};
+
+fn main() -> Result<(), ()> {
+    let mut dport = None;
+
+    for port in serialport::available_ports().unwrap() {
+        if let serialport::SerialPortType::UsbPort(serialport::UsbPortInfo {
+            serial_number: Some(sn),
+            ..
+        }) = &port.port_type
+        {
+            if sn.as_str() == "TESTssata" {
+                dport = Some(port.clone());
+                break;
+            }
+        }
+    }
+
+    let dport = if let Some(port) = dport {
+        port
+    } else {
+        eprintln!("Error: No `Pretty hal machine` connected!");
+        return Ok(());
+    };
+
+    let mut port = serialport::new(dport.port_name, 115200)
+        .timeout(Duration::from_millis(5))
+        .open()
+        .map_err(drop)?;
+
+    let mut last_send = Instant::now();
+
+    loop {
+        if last_send.elapsed() >= Duration::from_secs(1) {
+            let str = "hello!\n";
+            print!("TX: {}", str);
+            port.write_all(str.as_bytes()).unwrap();
+
+            last_send = Instant::now();
+        }
+    }
+}
