d59eeef2ecd7589e541c508f1fb1682adbd7d0f1
aissata <aimaiga2@gmail.com>
2022-08-02 14:04:36 +0200

    lights and temps change with GUI

--- host/src/bin/app.rs
+++ host/src/bin/app.rs
@@ -21,38 +21,94 @@ enum Response {
 pub enum USBError {
     BadCommand,
 }
+pub struct Board {
+    port: Box<dyn SerialPort>,
+}
+
+impl Board {
+    fn new() -> Option<Self> {
+        let mut dport = None;
+        for port in serialport::available_ports().unwrap() {
+            if let serialport::SerialPortType::UsbPort(serialport::UsbPortInfo {
+                serial_number: Some(sn),
+                ..
+            }) = &port.port_type
+            {
+                // Serial number must be same as in the firmware
+                if sn.as_str() == "InOurEyes" {
+                    dport = Some(port.clone());
+                    break;
+                }
+            }
+        }
+
+        let dport = dport?;
+        let port = serialport::new(dport.port_name, 115200)
+            .timeout(Duration::from_millis(5))
+            .open()
+            .map_err(drop)
+            .ok()?;
+        Some(Board { port })
+    }
+
+    fn toggle_light(&mut self, command: Command) -> Result<(), ()> {
+        let mut buf = [0; 64];
+
+        if let Ok(data) = to_slice_cobs(&command, &mut buf) {
+            self.port.write_all(data).unwrap();
+        }
+
+        if let Ok(count) = self.port.read(&mut buf) {
+            if let Ok(response) = from_bytes_cobs::<Response>(&mut buf[..count]) {
+                match response {
+                    Response::Ack => Ok(()),
+                    _ => Err(()),
+                }
+            } else {
+                Err(())
+            }
+        } else {
+            Err(())
+        }
+    }
+    fn get_temp(&mut self) -> Result<f32, ()> {
+        let mut buf = [0; 64];
+
+        // That returns the number of bytes
+        let command = Command::Temperature;
+        if let Ok(data) = to_slice_cobs(&command, &mut buf) {
+            self.port.write_all(data).unwrap();
+        }
+
+        if let Ok(count) = self.port.read(&mut buf) {
+            if let Ok(response) = from_bytes_cobs::<Response>(&mut buf[..count]) {
+                match response {
+                    Response::Temperature(t) => Ok(t),
+                    _ => Err(()),
+                }
+            } else {
+                Err(())
+            }
+        } else {
+            Err(())
+        }
+    }
+}
 
 fn main() {
     dioxus::desktop::launch(app);
 }
 
-fn get_temp(port: &mut Box<dyn SerialPort>) -> Result<f32, ()> {
-    let mut buf = [0; 64];
-
-    // That returns the number of bytes
-    let command = Command::Temperature;
-    if let Ok(data) = to_slice_cobs(&command, &mut buf) {
-        port.write_all(data).unwrap();
-    }
-
-    if let Ok(count) = port.read(&mut buf) {
-        if let Ok(response) = from_bytes_cobs::<Response>(&mut buf[..count]) {
-            match response {
-                Response::Temperature(t) => Ok(t),
-                _ => Err(()),
-            }
-        } else {
-            Err(())
-        }
-    } else {
-        Err(())
-    }
-}
-
+// App körs varje gång den renderas om
 fn app(cx: Scope) -> Element {
-    let mut port = init()?;
+    // ::new är en FnOnce
+    // let mut board = use_ref(&cx, Board::new);
+    let board = use_ref(&cx, || Board::new().unwrap());
     // smart pointer Rc<T>
-    let mut temp = use_state(&cx, || get_temp(&mut port).unwrap());
+    //let mut temp = **use_state(&cx, || board.write().get_temp().unwrap());
+
+    let temp = use_state(&cx, || board.write().get_temp().unwrap());
+    let is_on = use_state(&cx, || false);
     cx.render(rsx! (
         div {
             background_color: "orange",
@@ -60,37 +116,24 @@ fn app(cx: Scope) -> Element {
             p     {"Click on the buttons to have information from the board."}
         },
         button {
-            onclick: move |_| temp.modify(|_| get_temp(&mut port).unwrap()),
+            onclick: move |_| { temp.set(board.write().get_temp().unwrap()); },
             "Temperature!"
+        },
+        button {
+            onclick: move |_| {
+                    if **is_on {
+                        is_on.set(false);
+                        board.write().toggle_light(Command::Off).unwrap();
+                    } else {
+                        is_on.set(true);
+                        board.write().toggle_light(Command::On).unwrap();
+                    }
+
+                },
+            "Toggle light."
         },
         div {
             p  { "Temperature: {temp}" }
         }
-
     ))
-}
-fn init() -> Option<Box<dyn SerialPort>> {
-    let mut dport = None;
-    for port in serialport::available_ports().unwrap() {
-        if let serialport::SerialPortType::UsbPort(serialport::UsbPortInfo {
-            serial_number: Some(sn),
-            ..
-        }) = &port.port_type
-        {
-            // Serial number must be same as in the firmware
-            if sn.as_str() == "InOurEyes" {
-                dport = Some(port.clone());
-                break;
-            }
-        }
-    }
-
-    let dport = dport?;
-    let port = serialport::new(dport.port_name, 115200)
-        .timeout(Duration::from_millis(5))
-        .open()
-        .map_err(drop)
-        .ok()?;
-    println!("Possible instructions: \n on \n off \n temp");
-    Some(port)
 }
